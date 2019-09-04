79d245d72665d6e545c78d58a058d1241ae79bd0
Michael Riedel <michael.riedel@hybris.com>
2019-09-04 18:15:02 +0200

    Refactor towards dynamic CA management.

---
+++ ca/ca.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function reset {
+  local ca_base_dir="$( cd "$(dirname "$0")" ; pwd -P )"
+  local ca_name="$1"
+
+  (
+    cd "$ca_base_dir"
+    cd "$ca_name"
+
+    find . -type d -and -not -name '.' | xargs rm -r
+    find . -type f -and -not -name 'serial' -and -not -name 'ca-req.config' | xargs rm
+
+    mkdir 'newcerts'
+    mkdir 'private'
+    chmod go-rwx 'private'
+
+    (
+      set -x
+      openssl req -new -config ca-req.config -newkey rsa:4096 -passout env:TLS_PLAYGROUND_PASS -keyout private/ca-key.pem -out ca-csr.pem
+      openssl x509 -req -in ca-csr.pem -days 90 -signkey private/ca-key.pem -passin env:TLS_PLAYGROUND_PASS -out ca-cert.pem
+    )
+  )
+}
+
+export TLS_PLAYGROUND_PASS="${TLS_PLAYGROUND_PASS:=1234}"
+
+reset "$@"
