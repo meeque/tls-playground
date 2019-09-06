2746e208c5d2985995530b7b3c1968b43922a1b4
Michael Riedel <michael.riedel@hybris.com>
2019-09-06 20:53:01 +0200

    Add CA support for creating new cert requests.

--- ca/ca.sh
+++ ca/ca.sh
@@ -41,10 +41,10 @@ function sign {
   then
     if [[ "${csr_file}" ]]
     then
-      local csr_file_path="$( pwd -P )/${csr_file}"
+      local csr_file_path="$( cd "$(dirname "${csr_file}")" ; pwd -P )/$(basename "${csr_file}")"
       if [[ "${cert_link}" ]]
       then
-        local cert_link_path="$( pwd -P )/${cert_link}"
+        local cert_link_path="$( cd "$(dirname "${cert_link}")" ; pwd -P )/$(basename "${cert_link}")"
       fi
 
       (
@@ -77,6 +77,46 @@ function sign {
 
 
 
+function request {
+  local ca_name="$1"
+  local config_file="$2"
+
+  if [[ "${ca_name}" ]]
+  then
+    if [[ "${config_file}" ]]
+    then
+      local config_file_path="$( cd "$(dirname "${config_file}")" ; pwd -P )/$(basename "${config_file}")"
+      local config_file_basepath="$(dirname ${config_file_path})"
+      local config_name="$(basename ${config_file_path})"
+      local config_name="$( echo "${config_name}" | sed -e 's/[.]config$//' )"
+      local reqest_file_path="${config_file_basepath}/${config_name}-csr.pem"
+      local key_file_path="${config_file_basepath}/private/${config_name}-key.pem"
+      local cert_link_path="${config_file_basepath}/${config_name}-cert.pem"
+
+      echo "${config_file_basepath}" ' >>> ' "${config_name}"
+
+      mkdir -p "${config_file_basepath}/private"
+      rm "${reqest_file_path}" || true
+      rm "${key_file_path}" || true
+      rm "${cert_link_path}" || true
+      (
+        set -x
+        openssl req -new -config "${config_file_path}" -newkey rsa:2048 -passout env:TLS_PLAYGROUND_PASS -keyout "${key_file_path}" -out "${reqest_file_path}"
+      )
+
+      sign "${ca_name}" "${reqest_file_path}" "${cert_link_path}"
+    else
+      echo "No certificate request config file name specified. Specify the config file to request and sign!"
+      exit 1
+    fi
+  else
+    echo "No CA name specified. Specify the CA to sign with!"
+    exit 1
+  fi
+}
+
+
+
 export TLS_PLAYGROUND_PASS="${TLS_PLAYGROUND_PASS:=1234}"
 
 ca_base_dir="$( cd "$(dirname "$0")" ; pwd -P )"
@@ -87,7 +127,7 @@ command="$1"
 shift
 
 case "$command" in
-  'reset' | 'sign' )
+  'reset' | 'sign' | 'request' )
     "$command" "$@"
     ;;
   * )
