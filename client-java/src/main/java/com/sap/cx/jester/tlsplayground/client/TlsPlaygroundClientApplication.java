252f00e06b16b272891e0bbf35b1de6e38a059af
Michael Riedel <michael.riedel@hybris.com>
2019-09-11 15:56:51 +0200

    First crude implementation of a Java-based HTTP/TLS client.

--- client-java/src/main/java/com/sap/cx/jester/tlsplayground/client/TlsPlaygroundClientApplication.java
+++ client-java/src/main/java/com/sap/cx/jester/tlsplayground/client/TlsPlaygroundClientApplication.java
@@ -1,5 +1,13 @@
 package com.sap.cx.jester.tlsplayground.client;
 
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +22,16 @@ public class TlsPlaygroundClientApplication implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		System.out.println("Hello World!");
+		final List<String> nonOptionArgs = args.getNonOptionArgs();
+		if (nonOptionArgs.size() == 1) {
+			final String requestUrl = nonOptionArgs.get(0);
+
+			final HttpClient client = HttpClients.createDefault();
+
+			final HttpResponse response = client.execute(new HttpGet(requestUrl));
+			final InputStream responseEntityContentStream = response.getEntity().getContent();
+			IOUtils.copy(responseEntityContentStream, System.out);
+		}
 	}
 
 }
