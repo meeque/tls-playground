c2043bf8b47fc90b8252b1ae95e00747d4d2d687
Michael Riedel <michael.riedel01@sap.com>
2021-07-07 17:47:00 +0200

    Add tests for the java client application CLI.

--- client-java/src/test/java/com/sap/cx/jester/tlsplayground/client/TlsPlaygroundClientApplicationTests.java
+++ client-java/src/test/java/com/sap/cx/jester/tlsplayground/client/TlsPlaygroundClientApplicationTests.java
@@ -1,16 +1,57 @@
 package com.sap.cx.jester.tlsplayground.client;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import javax.net.ssl.SSLHandshakeException;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+
 public class TlsPlaygroundClientApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testDefaultSslContextWithGoodServerCertificate() {
+		runWithArgs("https://badssl.com");
+	}
+
+	@Test
+	public void testPkixSslContextWithGoodServerCertificate() {
+		runWithArgs("--tls.check-revocation=true", "https://badssl.com");
+	}
+
+	@Test
+	public void testDefaultSslContextWithRevokedServerCertificate() {
+		runWithArgs("https://revoked.badssl.com");
+	}
+
+	@Test
+	public void testPkixSslContextWithRevokedServerCertificate() {
+		runWithArgs(SSLHandshakeException.class, "--tls.check-revocation=true", "https://revoked.badssl.com");
+	}
+
+	private void runWithArgs(final String ... args) {
+		SpringApplication.run(TlsPlaygroundClientApplication.class, args);
+	}
+
+	private void runWithArgs(final Class<?> expectedExceptionCause, final String ... args) {
+		try {
+			runWithArgs(args);
+		} catch (final Exception e) {
+			final Throwable cause = e.getCause();
+			Assert.assertTrue(
+					"Expected SpringApplication to throw an IllegalStateException, but got " + e.getClass().getName() + " instead.",
+					e instanceof IllegalStateException);
+			Assert.assertNotNull(
+					"Expected SpringApplication to throw an Exception with a cause, but got no cause.",
+					cause);
+			Assert.assertTrue(
+					"Expected SpringApplication to throw an Exception caused by " + expectedExceptionCause.getName() + ", but got " + cause.getClass().getName() + " instead.",
+					expectedExceptionCause.isInstance(cause));
+			return;
+		}
+		Assert.fail("Expected exception to be thrown, but got none.");
+	}
 }
