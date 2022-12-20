21e3577372d147aa007cd6c019b31f2836fd5698
Michael Riedel <meeque@meeque.de>
2022-12-20 23:10:51 +0100

    Tweak config env-var names.

--- server-nginx/server-nginx.sh
+++ server-nginx/server-nginx.sh
@@ -12,14 +12,12 @@ function check-env {
   local status=0
   [[ "${TP_SERVER_DOMAIN}" =~ ^([-a-zA-Z0-9]+[.])*[-a-zA-Z0-9]+$ ]] \
       || { status=1; echo "[TP] Variable TP_SERVER_DOMAIN with value '${TP_SERVER_DOMAIN}' does not look like a DNS domain name!"; }
-  [[ "${TP_HTTP_LISTEN_ADDRESS}" =~ ^([*]|[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3})$ ]] \
-      || { status=1; echo "[TP] Variable TP_HTTP_LISTEN_ADDRESS with value '${TP_HTTP_LISTEN_ADDRESS}' does not look like an IP address!"; }
-  [[ "${TP_HTTP_LISTEN_PORT}" =~ ^[0-9]{1,5}$ ]] \
-      || { status=1; echo "[TP] Variable TP_HTTP_LISTEN_PORT with value '${TP_HTTP_LISTEN_PORT}' does not look like a network port number!"; }
-  [[ "${TP_HTTPS_LISTEN_ADDRESS}" =~ ^([*]|[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3})$ ]] \
-      || { status=1; echo "[TP] Variable TP_HTTPS_LISTEN_ADDRESS with value '${TP_HTTPS_LISTEN_ADDRESS}' does not look like an IP address!"; }
-  [[ "${TP_HTTPS_LISTEN_PORT}" =~ ^[0-9]{1,5}$ ]] \
-      || { status=1; echo "[TP] Variable TP_HTTPS_LISTEN_PORT with value '${TP_HTTPS_LISTEN_PORT}' does not look like a network port number!"; }
+  [[ "${TP_SERVER_LISTEN_ADDRESS}" =~ ^([*]|[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3})$ ]] \
+      || { status=1; echo "[TP] Variable TP_SERVER_LISTEN_ADDRESS with value '${TP_SERVER_LISTEN_ADDRESS}' does not look like an IP address!"; }
+  [[ "${TP_SERVER_HTTP_PORT}" =~ ^[0-9]{1,5}$ ]] \
+      || { status=1; echo "[TP] Variable TP_SERVER_HTTP_PORT with value '${TP_SERVER_HTTP_PORT}' does not look like a network port number!"; }
+  [[ "${TP_SERVER_HTTPS_PORT}" =~ ^[0-9]{1,5}$ ]] \
+      || { status=1; echo "[TP] Variable TP_SERVER_HTTPS_PORT with value '${TP_SERVER_HTTPS_PORT}' does not look like a network port number!"; }
   return "${status}"
 }
 
@@ -35,7 +33,7 @@ function configure {
     config_file="$( echo "${config_file_template}" | sed -e 's/.tmpl$//' )"
     echo -n "[TP] Generating ${config_file}... "
     cat "${config_file_template}" \
-        | envsubst '$TP_SERVER_DOMAIN,$TP_HTTP_LISTEN_ADDRESS,$TP_HTTP_LISTEN_PORT,$TP_HTTPS_LISTEN_ADDRESS,$TP_HTTPS_LISTEN_PORT' \
+        | envsubst '${TP_SERVER_DOMAIN},${TP_SERVER_LISTEN_ADDRESS},${TP_SERVER_HTTP_PORT},${TP_SERVER_HTTPS_PORT}' \
         > "${config_file}"
     echo "done."
   done
@@ -52,10 +50,9 @@ function clean {
 
 export TP_PASS="${TLS_PLAYGROUND_PASS:=1234}"
 export TP_SERVER_DOMAIN="${TP_SERVER_DOMAIN:=localhost}"
-export TP_HTTP_LISTEN_ADDRESS="${TP_HTTP_LISTEN_ADDRESS:=127.0.0.1}"
-export TP_HTTP_LISTEN_PORT="${TP_HTTP_LISTEN_PORT:=8080}"
-export TP_HTTPS_LISTEN_ADDRESS="${TP_HTTPS_LISTEN_ADDRESS:=127.0.0.1}"
-export TP_HTTPS_LISTEN_PORT="${TP_HTTPS_LISTEN_PORT:=8443}"
+export TP_SERVER_LISTEN_ADDRESS="${TP_SERVER_LISTEN_ADDRESS:=127.0.0.1}"
+export TP_SERVER_HTTP_PORT="${TP_SERVER_HTTP_PORT:=8080}"
+export TP_SERVER_HTTPS_PORT="${TP_SERVER_HTTPS_PORT:=8443}"
 
 server_nginx_base_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 
