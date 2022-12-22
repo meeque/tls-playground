2041cb08a20b231065c89da8bad034f0aef03c3b
Michael Riedel <michael.riedel01@sap.com>
2022-12-22 20:26:03 +0100

    Start acme module with certbot config and move nginx config for acme challanges there. Adjust server-ngnix.sh accordingly, including new env validation rules.

--- server-nginx/server-nginx.sh
+++ server-nginx/server-nginx.sh
@@ -10,6 +10,7 @@ port_regexp=''
 
 function check-env {
   local status=0
+
   [[ "${TP_SERVER_DOMAIN}" =~ ^([-a-zA-Z0-9]+[.])*[-a-zA-Z0-9]+$ ]] \
       || { status=1; echo "[TP] Variable TP_SERVER_DOMAIN with value '${TP_SERVER_DOMAIN}' does not look like a DNS domain name!"; }
   [[ "${TP_SERVER_LISTEN_ADDRESS}" =~ ^([*]|[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3})$ ]] \
@@ -18,6 +19,12 @@ function check-env {
       || { status=1; echo "[TP] Variable TP_SERVER_HTTP_PORT with value '${TP_SERVER_HTTP_PORT}' does not look like a network port number!"; }
   [[ "${TP_SERVER_HTTPS_PORT}" =~ ^[0-9]{1,5}$ ]] \
       || { status=1; echo "[TP] Variable TP_SERVER_HTTPS_PORT with value '${TP_SERVER_HTTPS_PORT}' does not look like a network port number!"; }
+
+  [[ "${TP_ACME_SERVER_URL}" =~ ^https:[/][/]([-a-zA-Z0-9]+[.])*[-a-zA-Z0-9]+(:[0-9]{1,5})?([/][-a-zA-Z0-9.+*_~]*)*$ ]] \
+      || { status=1; echo "[TP] Variable TP_ACME_SERVER_URL with value '${TP_ACME_SERVER_URL}' does not look like an absolute https url! Please note that user-info, query string, fragment, or exotic path characters are not allowed here!"; }
+  [[ "${TP_ACME_ACCOUNT_EMAIL}" =~ ^[-a-zA-Z0-9._%+]+@([-a-zA-Z0-9]+[.])*[-a-zA-Z0-9]+$ ]] \
+      || { status=1; echo "[TP] Variable TP_ACME_ACCOUNT_EMAIL with value '${TP_ACME_ACCOUNT_EMAIL}' does not look like an email address!"; }
+
   return "${status}"
 }
 
@@ -25,6 +32,7 @@ function check-env {
 
 function configure {
   cd "${server_nginx_base_dir}"
+  cd ..
 
   check-env || return
 
@@ -33,7 +41,7 @@ function configure {
     config_file="$( echo "${config_file_template}" | sed -e 's/.tmpl$//' )"
     echo -n "[TP] Generating ${config_file}... "
     cat "${config_file_template}" \
-        | envsubst '${TP_SERVER_DOMAIN},${TP_SERVER_LISTEN_ADDRESS},${TP_SERVER_HTTP_PORT},${TP_SERVER_HTTPS_PORT}' \
+        | envsubst '${TP_SERVER_DOMAIN},${TP_SERVER_LISTEN_ADDRESS},${TP_SERVER_HTTP_PORT},${TP_SERVER_HTTPS_PORT},${TP_ACME_SERVER_URL},${TP_ACME_ACCOUNT_EMAIL}' \
         > "${config_file}"
     echo "done."
   done
@@ -48,11 +56,13 @@ function clean {
 
 
 
-export TP_PASS="${TLS_PLAYGROUND_PASS:=1234}"
+export TP_PASS="${TP_PASS:=1234}"
 export TP_SERVER_DOMAIN="${TP_SERVER_DOMAIN:=localhost}"
 export TP_SERVER_LISTEN_ADDRESS="${TP_SERVER_LISTEN_ADDRESS:=127.0.0.1}"
 export TP_SERVER_HTTP_PORT="${TP_SERVER_HTTP_PORT:=8080}"
 export TP_SERVER_HTTPS_PORT="${TP_SERVER_HTTPS_PORT:=8443}"
+export TP_ACME_SERVER_URL="${TP_ACME_SERVER_URL:=https://acme-staging-v02.api.letsencrypt.org/directory}"
+export TP_ACME_ACCOUNT_EMAIL="${TP_ACME_ACCOUNT_EMAIL:=webmaster@example.net}"
 
 server_nginx_base_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 
