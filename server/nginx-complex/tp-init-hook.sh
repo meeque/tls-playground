b1f0a357af99ee95e79121a0163aa9799fbe5537
Michael Riedel <meeque@meeque.de>
2023-01-06 01:02:23 +0100

    Adjust init hook to recent changes to var name.

--- server/nginx-complex/tp-init-hook.sh
+++ server/nginx-complex/tp-init-hook.sh
@@ -1,11 +1,11 @@
 
 echo "[TP] Configuring trusted CAs for client certificates that the 'nginx-complex' demo server accepts..."
-local trusted_clients_cas_file="${TP_BASE_DIR}/server/nginx-complex/virtual/server2/tls/trusted-clients-cas.certs.pem"
+local trusted_clients_cas_file="${tp_base_dir}/server/nginx-complex/virtual/server2/tls/trusted-clients-cas.certs.pem"
 :> "${trusted_clients_cas_file}"
 
 for ca_name in $( tp_ca_list )
 do
-    local ca_root_cert="${TP_BASE_DIR}/ca/${ca_name}/ca-root.cert.pem"
+    local ca_root_cert="${tp_base_dir}/ca/${ca_name}/ca-root.cert.pem"
     if [[ -f "${ca_root_cert}" ]]
     then
         echo "[TP] Adding root certificate of CA '${ca_name}' to trusted clients CAs file '${trusted_clients_cas_file}'..."
@@ -21,7 +21,7 @@ if [[ ! -s ${trusted_clients_cas_file} ]]
 then
     echo
     echo "[TP] Looks like no trusted clients CAs are available. Using self-signed fallback certificate instead..."
-    local fallback_cert_path="${TP_BASE_DIR}/server/nginx-complex/virtual/server2/tls/trusted-clients-fallback"
+    local fallback_cert_path="${tp_base_dir}/server/nginx-complex/virtual/server2/tls/trusted-clients-fallback"
     tp_cert_selfsign "${fallback_cert_path}.cert.conf"
     echo "[TP] Adding self-signed fallback certificate to trusted clients CAs file '${trusted_clients_cas_file}'..."
     cat "${fallback_cert_path}.cert.pem" > "${trusted_clients_cas_file}"
