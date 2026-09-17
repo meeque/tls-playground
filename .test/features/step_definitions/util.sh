69b34d720dae00574bc8f2a46b0478a895838c99
Michael Riedel <meeque@meeque.de>
2026-09-17 18:22:55 +0200

    Move a @before step to the new shellkin support.sh file.
    Also make some util steps more reliable and more readable.

--- .test/features/step_definitions/util.sh
+++ .test/features/step_definitions/util.sh
@@ -1,17 +1,22 @@
-@BeforeAll
-  . ../bin/tp
+@Given {file_role} file `{file_path}`
 
-
-
-@Given {file_role} file `{file_name}`
-  run ls ${file_name}
+  run "ls \"${file_path}\""
+  [[ "${LAST_EXIT_CODE}" == '0' ]] || fail "Expected file \"${file_path}\" does not exits."
 
 
 
 @When I run `tp {command} {arg}`
-  run tp ${command} "${arg}"
+
+  run "tp ${command} \"${arg}\""
+
+
+
+@Then the command should succeed
+
+  [[ "${LAST_EXIT_CODE}" == '0' ]] || fail "Expected a commmand to run successfully, but it ran with exit code ${LAST_EXIT_CODE}."
 
 
 
 @Then `tp` should generate {file_role} file `{file_path}`
-  run ls "${file_path}"
+  run "ls \"${file_path}\""
+  [[ "${LAST_EXIT_CODE}" == '0' ]] || fail "Expected file \"${file_path}\" does not exits."
---
+++ .test/features/support.sh
@@ -0,0 +1,2 @@
+# import library functions from the tp script
+. ../bin/tp
