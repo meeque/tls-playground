34540140fa38ce6e8c0d7c6c8013a0ed8b0c481c
Michael Riedel <meeque@meeque.de>
2026-09-18 15:08:30 +0200

    Extract test support function tpt_extract_commands.
    The `tpt` prefix stands for "TLS Playground Test".

--- .test/features/step_definitions/util.sh
+++ .test/features/step_definitions/util.sh
@@ -18,15 +18,15 @@
 
 @Then TP should have run command `{command}`
 
-  prefixed_command_lines="$( echo "${LAST_STDOUT}" | grep -E '^\S+\$\S+\s+' )"
-  plain_command_lines="$( echo "${prefixed_command_lines}" | sed -E -e 's/^\S+\$\S+\s+//' )"
-  echo "${plain_command_lines}" | grep -F "${command}" > /dev/null || fail "Could not find the command logged in TP outputs"
+  tpt_extract_commands "${LAST_STDOUT}" | grep -F "${command}" > /dev/null || fail "Could not find the command logged in TP outputs"
+
 
 
 @Then {file_role} file `{file_path}` should exist
 
   run "ls \"${file_path}\""
   [[ "${LAST_EXIT_CODE}" == '0' ]] || fail "Expected file does not exit."
+
 
 
 @Then {file_role} file `{file_path}` should NOT exist
--- .test/features/support.sh
+++ .test/features/support.sh
@@ -1,2 +1,10 @@
 # import library functions from the tp script
 . ../bin/tp
+
+
+
+function tpt_extract_commands {
+  echo "$1" \
+    | grep -E '^\S+\$\S+\s+' || true \
+    | sed -E -e 's/^\S+\$\S+\s+//'
+}
