dc375e3ec8f732293f073dcd9efbf127fa12084f
Michael Riedel <meeque@meeque.de>
2026-09-20 14:36:08 +0200

    Use arithmethic valuation when matching exit codes in tests.

--- .test/features/step_definitions/cert.sh
+++ .test/features/step_definitions/cert.sh
@@ -1,7 +1,7 @@
 @Given all sample certificates have been cleaned
 
   run "tp cert clean"
-  [[ "${LAST_EXIT_CODE}" == '0' ]] \
+  (( LAST_EXIT_CODE == 0 )) \
     || fail "Precondition commmand \`tp cert clean\` has failed."
 
 
--- .test/features/step_definitions/util.sh
+++ .test/features/step_definitions/util.sh
@@ -6,14 +6,14 @@
 
 @Then the command should succeed
 
-  [[ "${LAST_EXIT_CODE}" == '0' ]] \
+  (( LAST_EXIT_CODE == 0 )) \
     || fail "The previous command ran with a non-zero exit code, indicating failure"
 
 
 
 @Then the command should fail
 
-  [[ "${LAST_EXIT_CODE}" != '0' ]] \
+  (( LAST_EXIT_CODE != 0 )) \
     || fail "The previous command ran with a zero exit code, indicating success"
 
 
