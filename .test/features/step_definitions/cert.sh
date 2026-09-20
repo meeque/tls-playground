e72c689d8cc168eed10db55d5d4083690fc0e8a7
Michael Riedel <meeque@meeque.de>
2026-09-20 12:35:50 +0200

    Reformat test steps for readability.

--- .test/features/step_definitions/cert.sh
+++ .test/features/step_definitions/cert.sh
@@ -1,28 +1,56 @@
 @Given all sample certificates have been cleaned
 
   run "tp cert clean"
-  [[ "${LAST_EXIT_CODE}" == '0' ]] || fail "Precondition commmand \`tp cert clean\` has failed."
+  [[ "${LAST_EXIT_CODE}" == '0' ]] \
+    || fail "Precondition commmand \`tp cert clean\` has failed."
 
 
 
 @Then CNs in CSR `{csr_file}` and config `{config_file}` should match
 
-  local config_cn="$( tp_util_get_config_values "${config_file}" 'CN' '.*' )"
-  [[ -n ${config_cn} ]] || fail "could not find a CN field in config file \`${config_file}\`" || return 1
+  local config_cn="$(
+    tp_util_get_config_values "${config_file}" 'CN' '.*'
+  )"
+  [[ -n ${config_cn} ]] \
+    || fail "could not find a CN field in config file \`${config_file}\`" \
+    || return 1
 
-  local csr_cn="$( tp cert show "${csr_file}" | grep -E '^(\s)+Subject:' | grep -o -E 'CN=(\S)+' | sed -e 's/^CN=//' )"
-  [[ -n "${csr_cn}" ]] || fail "could not find a CN field in CSR file \`${csr_file}\`" || return 1
+  local csr_cn="$(
+    tp cert show "${csr_file}" \
+      | grep -E '^(\s)+Subject:' \
+      | grep -o -E 'CN=(\S)+' \
+      | sed -e 's/^CN=//'
+  )"
+  [[ -n "${csr_cn}" ]] \
+    || fail "could not find a CN field in CSR file \`${csr_file}\`" \
+    || return 1
 
-  [[ "${csr_cn}" == "${config_cn}" ]] || fail "CN in CSR is \`${csr_cn}\`, but CN in certificate config is \`${config_cn}\`" || return 1
+  [[ "${csr_cn}" == "${config_cn}" ]] \
+    || fail "CN in CSR is \`${csr_cn}\`, but CN in certificate config is \`${config_cn}\`"
 
 
 
 @Then CNs in certificate `{cert_file}` and CSR `{csr_file}` should match
 
-  local csr_cn="$( tp cert show "${csr_file}" | grep -E '^(\s)+Subject:' | grep -o -E 'CN=(\S)+' | sed -e 's/^CN=//' )"
-  [[ -n "${csr_cn}" ]] || fail "could not find a CN field in CSR file \`${csr_file}\`" || return 1
+  local csr_cn="$(
+    tp cert show "${csr_file}" \
+      | grep -E '^(\s)+Subject:' \
+      | grep -o -E 'CN=(\S)+' \
+      | sed -e 's/^CN=//'
+  )"
+  [[ -n "${csr_cn}" ]] \
+    || fail "could not find a CN field in CSR file \`${csr_file}\`" \
+    || return 1
 
-  local cert_cn="$( tp cert show "${cert_file}" | grep -E '^(\s)+Subject:' | grep -o -E 'CN=(\S)+' | sed -e 's/^CN=//' )"
-  [[ -n ${cert_cn} ]] || fail "could not find a CN field in certificate file \`${cert_file}\`" || return 1
+  local cert_cn="$(
+    tp cert show "${cert_file}" \
+      | grep -E '^(\s)+Subject:' \
+      | grep -o -E 'CN=(\S)+' \
+      | sed -e 's/^CN=//'
+  )"
+  [[ -n ${cert_cn} ]] \
+    || fail "could not find a CN field in certificate file \`${cert_file}\`" \
+    || return 1
 
-  [[ "${cert_cn}" == "${csr_cn}" ]] || fail "CN in certificate is \`${cert_cn}\`, but CN in CSR is \`${csr_cn}\`" || return 1
+  [[ "${cert_cn}" == "${csr_cn}" ]] \
+    || fail "CN in certificate is \`${cert_cn}\`, but CN in CSR is \`${csr_cn}\`"
--- .test/features/step_definitions/util.sh
+++ .test/features/step_definitions/util.sh
@@ -6,74 +6,93 @@
 
 @Then the command should succeed
 
-  [[ "${LAST_EXIT_CODE}" == '0' ]] || fail "The previous command ran with a non-zero exit code, indicating failure"
+  [[ "${LAST_EXIT_CODE}" == '0' ]] \
+    || fail "The previous command ran with a non-zero exit code, indicating failure"
 
 
 
 @Then the command should fail
 
-  [[ "${LAST_EXIT_CODE}" != '0' ]] || fail "The previous command ran with a zero exit code, indicating success"
+  [[ "${LAST_EXIT_CODE}" != '0' ]] \
+    || fail "The previous command ran with a zero exit code, indicating success"
 
 
 
 @Then the command should print {number} lines of text
 
   num="$( echo "${LAST_STDOUT}" | wc -l )"
-  [[ "${num}" -eq "${number}" ]] || fail "Printed ${num} lines of text"
+  [[ "${num}" -eq "${number}" ]] \
+    || fail "Printed ${num} lines of text"
 
 
 
 @Then the command should print no text
 
-  [[ -z "${LAST_STDOUT}" ]] || fail "Did print more text than expected"
+  [[ -z "${LAST_STDOUT}" ]] \
+    || fail "Did print more text than expected"
 
 
 
 @Then the command should print "{output}"
 
-  [[ "${LAST_STDOUT}" == "${output}" ]] || fail "Did not print the expected text"
+  [[ "${LAST_STDOUT}" == "${output}" ]] \
+    || fail "Did not print the expected text"
 
 
 
 @Then TP should run command `{command}`
 
-  tpt_extract_commands "${LAST_STDOUT}" | grep -q -F "${command}" || fail "Could not find the command logged in TP outputs"
+  tpt_extract_commands "${LAST_STDOUT}" \
+    | grep -q -F "${command}" \
+      || fail "Could not find the command logged in TP outputs"
 
 
 
 @Then TP should print "{output}"
 
-  tpt_extract_tp_outputs "${LAST_STDOUT}" | grep -q -F "${output}" || fail "Could not find the expected text"
+  tpt_extract_tp_outputs "${LAST_STDOUT}" \
+    | grep -q -F "${output}" \
+      || fail "Could not find the expected text"
 
 
 
 @Then a sub-command should print "{output}"
 
-  tpt_extract_command_outputs "${LAST_STDOUT}" | grep -q -F "${output}" || fail "Could not find the expected text"
+  tpt_extract_command_outputs "${LAST_STDOUT}" \
+    | grep -q -F "${output}" \
+      || fail "Could not find the expected text"
 
 
 
 @Then a sub-command should print error "{error}"
 
-  tpt_extract_command_outputs "${LAST_STDERR}" | grep -q -F "${error}" || fail "Could not find the expected text"
+  tpt_extract_command_outputs "${LAST_STDERR}" \
+    | grep -q -F "${error}" \
+      || fail "Could not find the expected text"
 
 
 
 @Then {file_role} file `{file_path}` should exist
 
   run "ls \"${file_path}\""
-  [[ "${LAST_EXIT_CODE}" == '0' ]] || fail "Expected file does not exit."
+  [[ "${LAST_EXIT_CODE}" == '0' ]] \
+    || fail "Expected file does not exit."
 
 
 
 @Then {file_role} file `{file_path}` should NOT exist
 
   run "ls \"${file_path}\""
-  [[ "${LAST_EXIT_CODE}" != '0' ]] || fail "Unexpected file exits."
+  [[ "${LAST_EXIT_CODE}" != '0' ]] \
+    || fail "Unexpected file exits."
 
 
 
 @Then {number} files in `{directory}` should match wildcard pattern `{pattern}`
 
-  num="$( find "${directory}" -name "${pattern}" | wc -l )"
-  [[ "${num}" -eq "${number}" ]] || fail "${num} files matched the pattern"
+  num="$(
+    find "${directory}" -name "${pattern}" \
+      | wc -l
+  )"
+  [[ "${num}" -eq "${number}" ]] \
+    || fail "${num} files matched the pattern"
